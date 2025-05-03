291bf787af5e059b0e421ce9a96cbab885ebf23c
larissalotin <larissalt1234@gmail.com>
2025-05-03 20:32:37 -0300

    feat: Criação da API cor

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -3,6 +3,6 @@ from django.db import models
 class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
 
-     def __str__(self):
+    def __str__(self):
         return f"({self.id}) {self.descricao}"
     
